4f34bf335d83dff9459e2c29e8e7a892d22cedd8
antonioeatgoat <mangiacapra.antonio@gmail.com>
2020-03-10 23:23:43 +0100

    accepts symbol as a parameter

--- percentile.py
+++ percentile.py
@@ -1,7 +1,15 @@
 from api import alphavantage as api_interface
 from dotenv import load_dotenv
 import os
+import sys
 
 load_dotenv()
 
-print(api_interface.fetch('EXSA.DE', os.getenv("APIKEY")))
+
+if 1 < len(sys.argv):
+    symbol = sys.argv[1]
+else:
+    symbol = input('Please provide a symbol to analyse:\n')
+
+
+print(api_interface.fetch(symbol, os.getenv("APIKEY")))
