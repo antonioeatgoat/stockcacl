112f9ab0fbd732a0ebdda9dc107d463a7edb4174
antonioeatgoat <mangiacapra.antonio@gmail.com>
2020-03-19 18:23:10 +0100

    add setup file

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="stockcacl",
+    version="1.0.0",
+    author="Antonio Mangiacapra",
+    description="Stock Calculator - Percentile Analysis",
+    long_description="A simple python module to perform percentile analysis on stock data",
+    long_description_content_type="text/markdown",
+    url="https://github.com/antonioeatgoat/percentile-analysis",
+    packages=setuptools.find_packages(),
+    install_requires=['pandas', 'requests'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
