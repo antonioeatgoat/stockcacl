2b33b54786ed87965a78c3b4b8fdbac1d0554cd6
antonioeatgoat <mangiacapra.antonio@gmail.com>
2020-03-20 17:24:29 +0100

    update version to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="stockcacl",
-    version="0.1.0",
+    version="0.1.1",
     author="Antonio Mangiacapra",
     description="Stock Calculator - Percentile Analysis",
     long_description="A simple python module to perform percentile analysis on stock data",
