7647a4bc305ded4f2f3430af8ac0576abd0fe178
antonioeatgoat <mangiacapra.antonio@gmail.com>
2020-03-20 17:24:11 +0100

    add python dotenv as dependency

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/antonioeatgoat/stockcacl/",
     packages=setuptools.find_packages(),
-    install_requires=['pandas', 'requests'],
+    install_requires=['python-dotenv', 'pandas', 'requests'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
